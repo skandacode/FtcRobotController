0e760aecaa9971f38df52a785d46bb070fda0761
Arkacode1230 <92735305+Arkacode1230@users.noreply.github.com>
2024-04-14 13:35:24 -0500

    red auto 2+6

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,9 +15,59 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 50, Math.toRadians(90), Math.toRadians(70), 10.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(11.83, 62.16, Math.toRadians(270.00)))
-                                .splineTo(new Vector2d(8, 37), Math.toRadians(228.62))
-                                .lineToLinearHeading(new Pose2d(48, 29, Math.toRadians(180.00)))
+                        drive.trajectorySequenceBuilder(new Pose2d(11.83, -62.16, Math.toRadians(-270.00)))
+
+
+                                .lineTo(new Vector2d(25, -42))
+                                .setReversed(true)
+
+
+                                .back(4)
+                                .lineToLinearHeading(new Pose2d(48, -42, Math.toRadians(-180.00)))
+                                .setReversed(false)
+
+                                .waitSeconds(0.4)
+                                .lineToSplineHeading(new Pose2d(30, -12, Math.toRadians(-178)))
+                                .splineToConstantHeading(new Vector2d(20, -12), Math.toRadians(-178))
+
+                                .forward(42.5)//first pickup
+
+                                .setReversed(true)
+
+
+                                .lineTo(new Vector2d(30, -12))
+                                .splineToConstantHeading(new Vector2d(43, -35), -90)
+
+
+                                .lineToSplineHeading(new Pose2d(30, -12, Math.toRadians(-179)))
+                                .splineToConstantHeading(new Vector2d(20, -12), Math.toRadians(-179))
+
+                                .forward(44)//second pickup
+
+                                .waitSeconds(1)
+                                .setReversed(true)
+
+
+
+                                .lineTo(new Vector2d(30, -12))
+                                .splineToConstantHeading(new Vector2d(44, -35), -90)
+
+                                .waitSeconds(0.8)
+                                .setReversed(false)
+
+                                .lineToSplineHeading(new Pose2d(30, -12, Math.toRadians(-178)))
+                                .splineToConstantHeading(new Vector2d(20, -9), Math.toRadians(-178))
+                                .splineTo(new Vector2d(-26, -9), Math.toRadians(-160))//intake for third time
+
+
+
+
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(-9, -9, Math.toRadians(-180)), 0)
+                                .splineToSplineHeading(new Pose2d(30, -12, Math.toRadians(-180)), 0)
+                                .splineToSplineHeading(new Pose2d(44.3, -35, Math.toRadians(-180)), Math.toRadians(80))
+
+                                .waitSeconds(0.7)
                                 .build()
                 );
 
